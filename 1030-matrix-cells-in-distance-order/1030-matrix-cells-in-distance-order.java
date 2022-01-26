b64d7cb7628b7b58d7cdeb5358ceffadc7d3b006
Saikat Kar <saikat16th@gmail.com>
2022-01-26 20:20:01 +0530

    Time: 17 ms (45.40%), Space: 87.7 MB (16.71%) - LeetHub

---
+++ 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.java
@@ -0,0 +1,31 @@
+class Solution {
+    int directions [][] = new int[][]{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    public int[][] allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
+        int n = rows * cols;
+        int [][] result = new int[n][2];
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{rCenter, cCenter});
+        int k = 0;
+        boolean [][] visited = new boolean[rows][cols];
+        visited[rCenter][cCenter] = true;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size -- > 0) {
+                int [] arr = queue.poll();
+                //visited[arr[0]][arr[1]] = true;
+                //System.out.println(Arrays.toString(arr));
+                result[k++] = arr;
+                for (int i = 0; i < directions.length; i++) {
+                    int x = arr[0] + directions[i][0];
+                    int y = arr[1] + directions[i][1];
+                    if (x < 0 || y < 0 || x > rows-1 || y > cols-1 || visited[x][y]) {
+                        continue;
+                    }
+                    visited[x][y] = true;
+                    queue.add(new int[]{x, y});
+                }
+            }
+        }
+        return result;
+    }
+}
