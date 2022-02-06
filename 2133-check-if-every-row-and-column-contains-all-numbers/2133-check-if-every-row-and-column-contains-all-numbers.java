db7d1e85799fadce411299b310041dc6e82fb96f
Saikat Kar <saikat16th@gmail.com>
2022-02-06 22:30:09 +0530

    Time: 15 ms (63.09%), Space: 43.4 MB (80.77%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
@@ -11,7 +11,7 @@ class Solution {
             }
             set.clear();
         }
-        set.clear();
+        //set.clear();
         for (int i = 0 ; i < n ; i++) {
             for (int j = 0 ; j < n; j++) {
                 set.add(matrix[j][i]);
