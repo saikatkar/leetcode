9f20b7fa7341bc730d3b9286991bf50337aa348d
Saikat Kar <saikat16th@gmail.com>
2022-07-10 13:23:05 +0530

    Time: 97 ms (5.01%), Space: 43.7 MB (84.71%) - LeetHub

---
+++ 784-letter-case-permutation/784-letter-case-permutation.java
@@ -0,0 +1,28 @@
+class Solution {
+    Set<String> result = new HashSet<>();
+    public List<String> letterCasePermutation(String s) {
+        generate(s.toCharArray(), 0);
+        List<String> resultList = new ArrayList<>();
+        resultList.addAll(result);
+        return resultList;
+    }
+    private void generate (char [] sArr, int j)
+    {
+        result.add(new String(sArr));
+        for (int i = j; i < sArr.length; i++) {
+            if (sArr[i] >= 48 && sArr[i] <= 57) {
+                continue;
+            }
+            char ch = sArr[i];
+            if (ch >= 65 && ch <= 90) {
+                sArr[i] = (char)(ch + 32);
+                generate(sArr, i+1);
+                sArr[i] = ch;
+            } else {
+                sArr[i] = (char)(ch - 32);
+                generate(sArr, j+1);
+                sArr[i] = ch;
+            }
+        }
+    }
+}
