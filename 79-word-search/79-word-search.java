2e3e8b9318d15f30a94f71268d22ec14bc0d217d
Saikat Kar <saikat16th@gmail.com>
2022-07-29 23:31:29 +0530

    Time: 158 ms (61.71%), Space: 41.8 MB (78.48%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,53 +1,46 @@
 class Solution {
     int directions[][]=new int[][]{{0,1},{1,0},{-1,0},{0,-1}};
-    
-    public boolean exist(char[][] board, String word)
-    {
-      boolean visited [][] = new boolean [board.length][board[0].length];  
-      for (int i = 0; i < board.length; i++) {
-          for (int j = 0; j < board[0].length; j++) {
-              if (board[i][j] == word.charAt(0)) {
-                  if (searchWord(board, i, j, word, 0, visited)) {
-                      return true;
-                  }
-              }
-          }
-      }
-      return false;  
+    public boolean exist(char[][] board, String word) {
+        char [] wordArr = word.toCharArray();
+        boolean [][] visited = new boolean[board.length][board[0].length];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (wordArr[0] == board[i][j]) {
+                    if (dfs(board, i, j, visited, wordArr, 0)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
     
-    private boolean searchWord (char[][] board,int i, int j, String word, int k, boolean [][] visited)
-    { 
-       if (!isValid(board.length-1, board[0].length-1, visited, i, j)) {
-           return false;
-       }
-       if (k > word.length()-1) {
-           return false;
-       } 
-       if (word.charAt(k) == board[i][j] && k == word.length() -1) {
-           return true;
-       }
-       if (word.charAt(k) != board[i][j]) {
-           return false;
-       }
-       visited[i][j] = true; 
-       //System.out.println(board[i][j]) ;
-       for (int i1=0; i1 < directions.length; i1++) {
-            int x = i + directions[i1][0];
-            int y = j + directions[i1][1];
-            if (searchWord(board, x, y, word, k+1, visited)) {
-               return true; 
+    private boolean dfs (char[][] board, int i, int j,
+                         boolean [][] visited, char[] word, int k)
+    {
+        if (i < 0 || i > board.length-1 || j < 0 || j > board[0].length-1 ||
+            visited[i][j]) {
+            return false;
+        }
+        if (k > word.length-1) {
+            return false;
+        }
+        if (k == word.length-1 && word[k] == board[i][j]) {
+            return true;
+        }
+        if (word[k] != board[i][j]) {
+            return false;
+        }
+        visited[i][j] = true;
+        
+        for (int [] dir : directions) {
+            int x = i + dir[0];
+            int y = j + dir[1];
+            if (dfs(board, x, y, visited, word, k+1)) {
+                return true;
             }
         }
         visited[i][j] = false;
         return false;
-    }
-    
-    private boolean isValid (int row, int col, boolean [][] visited, int i, int j)
-    {
-        if (i > row || j > col || i < 0 || j < 0 || visited[i][j]) {
-            return false;
-        }
-        return true;
     }
 }
