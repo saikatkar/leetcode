82c0fe940d651fd9328198bd4ac7bbb31204f89e
Saikat Kar <saikat16th@gmail.com>
2022-09-17 18:49:55 +0530

    Time: 96 ms (46.36%), Space: 44.2 MB (5.12%) - LeetHub

---
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var increasingBST = function(root) {
+    let arr= InorderTraversal(root)
+    let rootNode=new TreeNode(arr[0],null,null)
+    root=rootNode;
+    for(let i=1;i<arr.length;i++){
+        if(arr[i]>arr[i-1]){
+            rootNode.left=null;
+            let r=new TreeNode(arr[i],null,null);
+            rootNode.right=r;
+            rootNode=rootNode.right
+        }
+    }
+   return root;
+};
+
+function InorderTraversal(root){
+    let arr=[];
+    if(root.left){
+        arr.push(...InorderTraversal(root.left))
+    }
+    arr.push(root.val)
+    if(root.right){
+        arr.push(...InorderTraversal(root.right))
+    }
+    
+    return arr;
+}
